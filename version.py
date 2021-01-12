7472dcb4413de0aef68ba0739beaebf710649921
Jason <jason.robock@gmail.com>
2021-01-11 17:50:22 -0700

    Final commit

--- version.py
+++ version.py
@@ -1,4 +1,4 @@
 import requests
-r = requests.get('http://www.google.com/')
+r = requests.get('https://raw.githubusercontent.com/Vinen88/CMPUT404_Lab1/main/version.py')
 print(r.text)
 print("Requests version:",requests.__version__)
